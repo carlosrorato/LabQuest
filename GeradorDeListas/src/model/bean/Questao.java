4098d47dbd9a78f46c0c80b5738b12cffec68678
Carlos Henrique Rorato Souza <carlosrorato2014@gmail.com>
2017-07-09 19:31:30 -0300

    criação da classe Questao

---
+++ GeradorDeListas/src/model/bean/Questao.java
@@ -0,0 +1,71 @@
+package model.bean;
+
+
+public class Questao {
+    private int id;
+    private int ano;
+    private String disciplina;
+    private String conteudo;
+    private String enunciado;
+    private String fonte;
+    private String gabarito;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public String getFonte() {
+        return fonte;
+    }
+
+    public void setFonte(String fonte) {
+        this.fonte = fonte;
+    }
+
+    public String getGabarito() {
+        return gabarito;
+    }
+
+    public void setGabarito(String gabarito) {
+        this.gabarito = gabarito;
+    }
+    
+    
+    
+}
