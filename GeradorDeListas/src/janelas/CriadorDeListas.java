4b4d01a98d18082ec76c3acdd9d409970f090387
Carlos Henrique Rorato Souza <carlosrorato2014@gmail.com>
2017-07-10 00:20:53 -0300

    modificações na interface visual do gerador de provas

--- GeradorDeListas/src/janelas/CriadorDeListas.java
+++ GeradorDeListas/src/janelas/CriadorDeListas.java
@@ -82,13 +82,13 @@ public class CriadorDeListas extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Id", "Disciplina", "Conteúdo", "Enunciado", "Fonte", "Ano", "Gabarito"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
@@ -194,15 +194,20 @@ public class CriadorDeListas extends javax.swing.JFrame {
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Nº", "Fonte", "Ano", "Enunciado", "Gabarito"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane3.setViewportView(jTable2);
 
         jButton8.setText("^");
